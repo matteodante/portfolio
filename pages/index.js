e809065d709f8ccf7739fa6d9ba4e57c21190ffa
Matteo Dante <matteo.dante659@gmail.com>
2023-04-02 02:49:03 +0200

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import { Suspense } from 'react'
-import { Canvas } from "@react-three/fiber";
+import * as THREE from 'three'
+import { Suspense, useRef } from 'react'
+import { useGLTF, Environment, CameraControls, Sky, Float  } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { EffectComposer, Bloom, DepthOfField } from '@react-three/postprocessing'
+import { easing } from 'maath'
 
 export default function Home() {
   return (
@@ -7,21 +11,39 @@ export default function Home() {
       <Suspense fallback={null}>
         <Canvas shadows >
           <Scene />
+          <CameraControls makeDefault />
+          <Sky inclination={0.52} scale={20} />
         </Canvas>
       </Suspense>
     </main>
   );
 }
 
-function Scene() {
+
+function Scene(props) {
+  const { nodes, materials } = useGLTF('/livingroom.glb')
   return (    
-    <>  
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+    <> 
+      <Sphere position={[-2, 2, -8]} scale={0.8} />
     </>  
   );
 }
+
+
+function Sphere({ color = "hotpink", floatIntensity = 15, position = [0, 0, 0], scale = 1 }) {
+  return (
+    <Float floatIntensity={floatIntensity}>
+      <mesh castShadow position={position} scale={scale}>
+        <sphereGeometry />
+        <meshBasicMaterial color={color} roughness={1} />
+      </mesh>
+    </Float>
+  )
+}
+
+
+
+
+
+  
+useGLTF.preload('/bedroom.glb')
