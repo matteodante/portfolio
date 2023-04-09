fd110212398f6b2dc8dc0d2ba55643c840b4d1ac
Matteo Dante <matteo.dante659@gmail.com>
2023-04-09 12:15:29 +0200

    Initial scale

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,17 @@
 import Canvas from "@/components/Scene";
 import { Overlay } from "@/components/Overlay";
 import { Loader } from "@react-three/drei";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
+      </Head>
       <Canvas />
       <Overlay />
     </>
